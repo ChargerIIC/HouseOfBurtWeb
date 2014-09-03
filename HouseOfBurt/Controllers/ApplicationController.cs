403c1adb1e0c12cf55eb2d7da837b647fbe8f6e2
HouseOfBurt <ChargerIIC@yahoo.com>
2014-09-02 20:37:26 -0700

    Refresh on Logo click

--- HouseOfBurt/Controllers/ApplicationController.cs
+++ HouseOfBurt/Controllers/ApplicationController.cs
@@ -9,17 +9,11 @@ namespace HouseOfBurt.Controllers
 {
     public class ApplicationController : Controller
     {
-        // GET: Application
-        public ActionResult Index()
-        {
-            return View();
-        }
+        #region Class Level Variables
 
-        public ActionResult ContextIsNeeded()
-        {
-            var questions = DataService.Instance.Database.ContextIsNeededQuestions.ToArray();
+        private ContextIsNeeded_Question[] questions = DataService.Instance.Database.ContextIsNeededQuestions.ToArray();
 
-            var images = new List<Tuple<string, int, int>>
+        List<Tuple<string, int, int>> images = new List<Tuple<string, int, int>>
             {
                 new Tuple<string, int, int>("../Content/img/BuisnessSuitWonder.png",20,65),
                 new Tuple<string, int, int>("../Content/img/ComputerAsk.png", 20,65),
@@ -28,6 +22,16 @@ namespace HouseOfBurt.Controllers
                 new Tuple<string, int, int>("../Content/img/LadyWonder.png", 30,55)
             };
 
+        #endregion Class Level Variables
+
+        // GET: Application
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ContextIsNeeded()
+        {
             var random = new Random();
             var image = images[random.Next(images.Count)];
             ViewBag.Question = questions[random.Next(questions.Length)];
@@ -35,6 +39,26 @@ namespace HouseOfBurt.Controllers
             ViewBag.QuestionTop = image.Item2;
             ViewBag.QuestionLeft = image.Item3;
             return View();
+        }
+
+        public JsonResult ContextNeededEndpoint()
+        {
+            var json = new JsonResult();
+            var random = new Random();
+            var image = images[random.Next(images.Count)];
+            var question = questions[random.Next(questions.Length)];
+
+            json.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+            json.Data = new
+            {
+                url = image.Item1,
+                top = image.Item2,
+                left = image.Item3,
+                caption = question.Caption,
+                link = question.Url
+            };
+
+            return json;
         }
     }
 }
