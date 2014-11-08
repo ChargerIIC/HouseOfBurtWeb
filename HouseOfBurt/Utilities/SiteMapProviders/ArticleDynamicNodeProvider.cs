451847543c43f4ce883cfdbb82ea1eba042ad2e9
HouseOfBurt <ChargerIIC@yahoo.com>
2014-11-07 19:00:59 -0800

    Dynamic SiteMap Implmentation

---
+++ HouseOfBurt/Utilities/SiteMapProviders/ArticleDynamicNodeProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HouseOfBurt.Models;
+using MvcSiteMapProvider;
+
+namespace HouseOfBurt.Utilities.SiteMapProviders
+{
+    public class ArticleDynamicNodeProvider : DynamicNodeProviderBase 
+    {
+        public override IEnumerable<DynamicNode> GetDynamicNodeCollection(ISiteMapNode node)
+        {
+            var articles = DataService.Instance.Database.Articles;
+                // Create a node for each album 
+                foreach (var article in articles)
+                {
+                    var dynamicNode = new DynamicNode {Title = article.Title};
+                    dynamicNode.RouteValues.Add("id", article.Slug);
+                    //dynamicNode.ParentKey = "Genre_" + album.Genre.Name;
+
+                    yield return dynamicNode;
+                }
+        } 
+    }
+}
---
+++ HouseOfBurt/Utilities/SiteMapProviders/ProductDynamicNodeProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HouseOfBurt.Models;
+using MvcSiteMapProvider;
+
+namespace HouseOfBurt.Utilities.SiteMapProviders
+{
+    public class ProductDynamicNodeProvider : DynamicNodeProviderBase 
+    {
+        public override IEnumerable<DynamicNode> GetDynamicNodeCollection(ISiteMapNode node)
+        {
+            var products = DataService.Instance.Database.Products;
+            // Create a node for each album 
+            foreach (var product in products)
+            {
+                var dynamicNode = new DynamicNode { Title = product.Name };
+                dynamicNode.RouteValues.Add("id", product.Slug);
+                //dynamicNode.ParentKey = "Genre_" + album.Genre.Name;
+
+                yield return dynamicNode;
+            }
+        } 
+    }
+}
